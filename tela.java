ae6786ea1049c349044685c5a8fac956cb359c8e
Kellen Padilha Silva <105509933+kellen-padilha@users.noreply.github.com>
2022-06-24 17:31:30 -0300

    MISSAO 8
    ALEGRIA!!!!

---
+++ tela.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Calculadora;
+
+/**
+ *
+ * @author User
+ */
+class tela {
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setText(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
